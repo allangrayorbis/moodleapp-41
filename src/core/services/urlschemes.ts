5b89d7af818356dbb5e80d2419cdefa6a4da6acd
Liam Beeton <liam.beeton@gmail.com>
2023-01-23 08:51:10 +0200

    config: rename app and url scheme

--- src/core/services/urlschemes.ts
+++ src/core/services/urlschemes.ts
@@ -223,7 +223,7 @@ export class CoreCustomURLSchemesProvider {
 
     /**
      * Get the data from a custom URL scheme. The structure of the URL is:
-     * moodlemobile://username@domain.com?token=TOKEN&privatetoken=PRIVATETOKEN&redirect=http://domain.com/course/view.php?id=2
+     * onlinecampus://username@domain.com?token=TOKEN&privatetoken=PRIVATETOKEN&redirect=http://domain.com/course/view.php?id=2
      *
      * @param url URL to treat.
      * @returns Promise resolved with the data.
